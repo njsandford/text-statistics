837b96781e3125bd6553fa35f687c6aed38acc1e
natalie <nataliesandford@hotmail.co.uk>
2018-03-05 11:38:37 +0000

    Implemented method to get the total number of lines from a text file. Added junit test to check output of method is as expected for a text file with paragraphs and for a blank text file.

--- src/main/java/com/njsandford/stats/TextStatistics.java
+++ src/main/java/com/njsandford/stats/TextStatistics.java
@@ -2,26 +2,40 @@ package com.njsandford.stats;
 
 import com.njsandford.io.ReadFile;
 
+import java.util.List;
+
 /**
  * Provides functionality to compute statistics on text files.
  */
 public class TextStatistics {
 
-    private String text;
+    private ReadFile readFile = new ReadFile();
 
-    public TextStatistics(String filepath) {
-        ReadFile readFile = new ReadFile();
-        this.text = readFile.getContentAsString(filepath);
+
+    public TextStatistics() {
+        this.readFile = new ReadFile();
     }
 
     /**
+     * @param filepath the full filepath in the text file.
      * @return number of whitespace delimited words in text document.
      */
-    public int wordCountByWhiteSpace() {
+    public int wordCountByWhiteSpace(String filepath) {
+        String text = readFile.getContentAsString(filepath);
         if (text == "") {
             return 0;
         }
         String[] words = text.split("\\s");
         return words.length;
+    }
+
+    /**
+     * @param filepath the full filepath of the text file.
+     * @return the number of lines within the text file.
+     */
+    public int lineCount(String filepath) {
+        List<String> lines = readFile.getLines(filepath);
+
+        return lines.size();
     }
 }
--- src/test/java/TextStatisticsTest.java
+++ src/test/java/TextStatisticsTest.java
@@ -5,16 +5,29 @@ public class TextStatisticsTest {
 
     @Test
     public void canCountWordsInTextFile() {
-        TextStatistics textStatistics = new TextStatistics("/Users/natalie/IdeaProjects/text-statistics/src/main/resources/files/test_words.txt");
-        int wordCount = textStatistics.wordCountByWhiteSpace();
+        TextStatistics textStatistics = new TextStatistics();
+        int wordCount = textStatistics.wordCountByWhiteSpace("/Users/natalie/IdeaProjects/text-statistics/src/main/resources/files/test_words.txt");
         int expectedWordCount = 19;
 
         Assert.assertEquals(expectedWordCount, wordCount);
 
-        textStatistics = new TextStatistics("/Users/natalie/IdeaProjects/text-statistics/src/main/resources/files/blank_file.txt");
-        wordCount = textStatistics.wordCountByWhiteSpace();
+        wordCount = textStatistics.wordCountByWhiteSpace("/Users/natalie/IdeaProjects/text-statistics/src/main/resources/files/blank_file.txt");
         expectedWordCount = 0;
 
         Assert.assertEquals(expectedWordCount, wordCount);
+    }
+
+    @Test
+    public void canGetLineCount() {
+        TextStatistics textStatistics = new TextStatistics();
+        int lineCount = textStatistics.lineCount("/Users/natalie/IdeaProjects/text-statistics/src/main/resources/files/test_file.txt");
+        int expectedLineCount = 9;
+
+        Assert.assertEquals(expectedLineCount, lineCount);
+
+        lineCount = textStatistics.lineCount("/Users/natalie/IdeaProjects/text-statistics/src/main/resources/files/blank_file.txt");
+        expectedLineCount = 0;
+
+        Assert.assertEquals(expectedLineCount, lineCount);
     }
 }
